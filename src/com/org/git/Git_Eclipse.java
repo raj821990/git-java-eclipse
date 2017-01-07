037edef4fd92c824367430da98d8206f7e1eed9e
Raj <amit.raj91@gmail.com>
2017-01-07 12:20:15 +0530

    Added two integer variable and add both of this.
    Change-Id: Iaa351e7785ca6a375bea16b3b2a4f4006bb98ff8
    Signed-off-by: Raj <amit.raj91@gmail.com>

--- src/com/org/git/Git_Eclipse.java
+++ src/com/org/git/Git_Eclipse.java
@@ -5,7 +5,9 @@ public class Git_Eclipse {
 	//Main Method........
 	public static void main(String[] args) {
 		System.out.println("This is the first Sysout for Git");
-
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
 
 }
